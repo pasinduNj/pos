210bb793070f5c9638b3b85d8fdc357c30eceb7c
Pasindu Jay <124522613+pasinduNj@users.noreply.github.com>
2023-03-03 18:41:36 +0530

    test

---
+++ 1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+float width,height,area;
+printf("Display area calculation of rectangle\n\n");
+printf("Enter the width(m) :");
+scanf("%f",&width);
+printf("Enter the height(m):");
+scanf("%f",&height);
+area=width*height;
+printf("Area of the rectangle:""%f",area);
+return 0;
+}
